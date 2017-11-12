6cdeb876149d1167a103b27ccab294ce445c5adf
RudyRed <willgross94@gmail.com>
2017-11-11 18:59:02 -0800

    pokemon show up

--- react-client/src/components/PokeDropList.jsx
+++ react-client/src/components/PokeDropList.jsx
@@ -4,19 +4,30 @@ class PokeDropList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: true
+      pokemon: ''
     }
+  }
+
+  changeHandler(event) {
+    this.setState({pokemon: event.target.value});
+  }
+
+  clickHandler(event) {
+    event.preventDefault();
+    this.props.handleSubmit(this.state.pokemon, this.props.player);
   }
 
   render() {
     return  ( <div className="playerone">
-      {this.state.display && (<form><select>
+      <form>Player {this.props.player}:<select value={this.state.value} onChange={this.changeHandler.bind(this)}>
         <option value="" hidden="hidden">Select your option</option>
-        {this.props.pokeBox.map(pokemon => <option value={pokemon.name}>{pokemon.id}: {pokemon.name}</option>)}
-      </select> <input type="submit" value="Confirm Pokemon Choice"/> </form>)}
+        {this.props.pokeBox.map(pokemon => <option value={pokemon.id}>{pokemon.id}: {pokemon.name}</option>)}
+      </select> <input type="submit" value="Confirm Pokemon Choice" onClick={this.clickHandler.bind(this)}/> </form>
     </div>)
   }
 
 }
+
+
 
 export default PokeDropList;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -15,14 +15,31 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    if (!this.state.pokeBox.length) {
+      $.ajax({
+        url: '/pokemon',
+        success: (data) => {
+          this.setState({pokeBox: data})
+        },
+        error: (err) => {
+          console.log('err', err);
+        }
+      });
+    }
+  }
+  pokemonSelectionSubmit(pokemonId, player) {
+    console.log('test')
+    console.log(pokemonId)
+    console.log(this)
     $.ajax({
-      url: '/pokemon',
+      url: this.state.pokeBox[pokemonId -1].url,
       success: (data) => {
         console.log(typeof data)
-        console.log(data)
-        this.setState({
-          pokeBox: data
-        })
+        if (player === 'One') {
+          this.setState({player1: data})
+        } else {
+          this.setState({player2: data})
+        }
       },
       error: (err) => {
         console.log('err', err);
@@ -30,13 +47,15 @@ class App extends React.Component {
     });
   }
 
-  render () {
+  render() {
     return (<div>
       <h1>Item List</h1>
-      Player One: <PokeDropList pokeBox={this.state.pokeBox}/>
-      Player Two: <PokeDropList pokeBox={this.state.pokeBox}/>
+      {!this.state.player1 && (<PokeDropList player={'One'} pokeBox={this.state.pokeBox} handleSubmit={this.pokemonSelectionSubmit.bind(this)}/>)}
+      {!this.state.player2 && (<PokeDropList player={'Two'} pokeBox={this.state.pokeBox} handleSubmit={this.pokemonSelectionSubmit.bind(this)}/>)}
+      {this.state.player1 && (<img src={this.state.player1.sprites.back_default} />)}
+      {this.state.player2 && (<img src={this.state.player2.sprites.front_default} />)}
     </div>)
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
