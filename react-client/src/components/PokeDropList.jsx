bfaa0039c0a7c4acf378812046fe012669fd514c
RudyRed <willgross94@gmail.com>
2017-11-11 16:06:39 -0800

    added a display property

--- react-client/src/components/PokeDropList.jsx
+++ react-client/src/components/PokeDropList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-// import ListItem from './ListItem.jsx';
 
-const PokeDropList = (props) => (
-  <div class="dropdown">
-    <select>
-      <option value="" disabled selected>Select your option</option>
-      {props.pokeBox.map(pokemon => <option value={pokemon.name}>{pokemon.id}: {pokemon.name}</option>)}
-    </select>
-  </div>
-)
-
-export default PokeDropList;
+class PokeDropList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+  }
+  render() {
+    return  ( <div className="playerone">
+      {this.state.display && (<select>
+        <option value="" hidden="hidden">Select your option</option>
+        {this.props.pokeBox.map(pokemon => <option value={pokemon.name}>{pokemon.id}: {pokemon.name}</option>)}
+      </select>)}
+    </div>)
+  }
+}
+  export default PokeDropList;
